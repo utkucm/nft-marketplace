b5a34675167806162a393c596a922366b8601030
Utku Çam <utkucam2@gmail.com>
2022-06-13 17:20:58 +0300

    removed Hello World

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello World!!</h1>
+      <h1 className="text-3xl font-bold underline"></h1>
     </div>
   );
 };
